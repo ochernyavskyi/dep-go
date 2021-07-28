e4ecc17341af7f114f4e72a7d2390114c2ed5d1a
Cherniavskyi Oleh <ochernyavskyi@gmail.com>
2021-07-28 15:23:12 +0300

    first commit

--- my-app.go
+++ my-app.go
@@ -10,16 +10,6 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("index.html")
-
-	router.GET("/", HelloWorld)
-	err := router.Run(":80")
-	if err != nil {
-		fmt.Print("fk u linter hehe")
-	}
-
-// func main() {
-//     msgHandler := msg("Hello World 2 !")
-//     http.ListenAndServe("0.0.0.0:80", msgHandler)
-// }
+    msgHandler := msg("Hello World 2 !")
+    http.ListenAndServe("0.0.0.0:80", msgHandler)
+}
