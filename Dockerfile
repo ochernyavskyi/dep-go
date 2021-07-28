15590971ca0d79cf354987b5a6896de614269888
Cherniavskyi Oleh <ochernyavskyi@gmail.com>
2021-07-28 15:04:47 +0300

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,43 @@
-FROM golang
 
-WORKDIR /go/src/app
-COPY my-app.go .
-RUN go build my-app.go
+FROM golang:alpine AS builder
+
+
+
+ENV GO111MODULE=on \
+    CGO_ENABLED=0 \
+    GOOS=linux \
+    GOARCH=amd64
+
+WORKDIR /build
+
+
+COPY . .
+
+
+
+
+RUN go mod download && \
+    go build -o main .
+
+
+
+
+FROM scratch
+
+
+COPY --from=builder /build/main /build/index.html ./
+
+
 EXPOSE 80
-CMD ["./my-app"]
+
+
+
+ENTRYPOINT ["/main"]
+
+#FROM golang
+#
+#WORKDIR /go/src/app
+#COPY my-app.go .
+#RUN go build my-app.go
+#EXPOSE 80
+#CMD ["./my-app"]
--- my-app.go
+++ my-app.go
@@ -1,14 +1,40 @@
 package main
-import (
-    "net/http"
-    "fmt"
-    )
 
-type msg string
-func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-   fmt.Fprint(resp, m)
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HelloWorld(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{"title": "Hellow world"})
 }
+
 func main() {
-    msgHandler := msg("Hello World 2 !")
-    http.ListenAndServe("0.0.0.0:80", msgHandler)
+	router := gin.Default()
+	router.LoadHTMLGlob("index.html")
+
+	router.GET("/", HelloWorld)
+	err := router.Run(":80")
+	if err != nil {
+		fmt.Print("fk u linter hehe")
+	}
+
 }
+
+
+// package main
+// import (
+//     "net/http"
+//     "fmt"
+//     )
+//
+// type msg string
+// func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+//    fmt.Fprint(resp, m)
+// }
+// func main() {
+//     msgHandler := msg("Hello World 2 !")
+//     http.ListenAndServe("0.0.0.0:80", msgHandler)
+// }
