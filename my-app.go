ab6a6cf0534dbbc008806bf943e25543f0733491
Cherniavskyi Oleh <ochernyavskyi@gmail.com>
2021-07-28 14:37:06 +0300

    first commit

--- my-app.go
+++ my-app.go
@@ -9,6 +9,6 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
    fmt.Fprint(resp, m)
 }
 func main() {
-    msgHandler := msg("Hello World 2 !")
+    msgHandler := msg("Hello World 2 ")
     http.ListenAndServe("0.0.0.0:80", msgHandler)
 }
